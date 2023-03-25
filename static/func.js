a7044e7019cc59a0046fee6581e6f3af8ce72ece
Takinda <gghanimalokaily@gmail.com>
2023-03-26 01:39:03 +0300

    login & sign up

--- static/func.js
+++ static/func.js
@@ -1,9 +1,13 @@
-let loginbtn = document.getElementsByClassName('subm');
-let signupbtn = document.getElementsByClassName("change");
+let loginbtn = document.getElementById('loginbtn');
+let signupbtn=document.getElementById('signup');
 let username = document.getElementsByClassName('usern')[0].innerHTML;
-let title = document.getElementsByClassName("title")
+let title = document.getElementsByClassName('title')
 let pas=document.getElementById('password').innerHTML
 function sign(){
+    document.getElementById('showname').style.display='none';
+    document.getElementById('confirm_password').style.display='none';
+    document.getElementById("login").style.height="300px";
+    document.getElementById('title').innerHTML="LOGIN";
     fetch("/../login",{
         body:({
             name:username,password:pas
@@ -11,4 +15,16 @@ function sign(){
             "Content-Type": "application/json"
         })
     })
+
+}
+
+function changer(){
+    
+}
+function sbtn(){
+    document.getElementById('showname').style.display='block';
+    document.getElementById('confirm_password').style.display='block';
+    document.getElementById("login").style.height="400px";
+    document.getElementById('title').innerHTML="SIGN UP";
+
 }
